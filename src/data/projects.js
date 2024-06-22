5f00743d93271ebcf13373e98a7b9b384a362843
Shrayash Shukla <ishrayash@gmail.com>
2024-06-22 20:05:41 +0530

    22july

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,107 +1,42 @@
 export const projects = [
   {
     id: 1,
-    name: "Accordion",
-    path: "accordion",
-  },
-  {
-    id: 2,
-    name: "Random Color Generator",
-    path: "color-generator",
-    tags: ['project']
-  },
-  {
-    id: 3,
-    name: "Star Rating",
-    path: "star-rating",
-  },
-  {
-    id: 4,
     name: "Image Slider",
     path: "image-slider",
   },
   {
-    id: 5,
+    id: 2,
     name: "Load More Button",
     path: "load-more-data",
-    tags: ['project']
+    tags: ["project"],
   },
   {
-    id: 6,
-    name: "Tree View",
-    path: "tree-view",
-  },
-  {
-    id: 7,
-    name: "QR Code Generator",
-    path: "qr-code-generator",
-    tags: ['project']
-  },
-  {
-    id: 8,
-    name: "Light Dark Mode",
-    path: "light-dark-mode",
-  },
-  {
-    id: 9,
-    name: "Custom Tabs",
-    path: "custom-tabs",
-  },
-  {
-    id: 10,
-    name: "Modal Popup",
-    path: "modal-popup",
-  },
-  {
-    id: 11,
-    name: "Github Profile Finder",
-    path: "github-profile-finder",
-    tags: ['project']
-  },
-  {
-    id: 12,
-    name: "Search Auto-Complete",
-    path: "search-auto-complete",
-  },
-  {
-    id: 13,
+    id: 3,
     name: "Tic Tac Toe",
     path: "tic-tac-toe",
-    tags: ['project']
+    tags: ["project"],
   },
   {
-    id: 14,
+    id: 4,
     name: "Feature Flags",
     path: "feature-flags",
   },
   {
-    id: 15,
-    name: "useFetch",
-    path: "use-fetch",
-    tags: ["custom hooks"],
-  },
-  {
-    id: 16,
+    id: 5,
     name: "useOutsideClick",
     path: "use-outside-click",
     tags: ["custom hooks"],
   },
   {
-    id: 17,
+    id: 6,
     name: "useWindowResize",
     path: "use-window-resize",
     tags: ["custom hooks"],
   },
   {
-    id: 18,
-    name: 'Scroll to particular section',
-    path: 'scroll-to-particular-section',
-    tags: ['project'],
+    id: 7,
+    name: "Weather App",
+    path: "weather-app",
+    tags: ["project"],
   },
-  {
-    id: 19,
-    name: 'Weather App',
-    path: 'weather-app',
-    tags: ['project']
-  }
 ];
